1b1febbb20a322107207efeed7722448b304ab24
VaradNashikkar <136262207+varadnb18@users.noreply.github.com>
2024-11-27 16:45:59 +0530

    do it

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(router);
 
-console.log(process.env.DB_PASS);
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
